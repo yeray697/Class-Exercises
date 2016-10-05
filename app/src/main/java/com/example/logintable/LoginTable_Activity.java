fc87ab3965a2fdbdff555360c46efa0b84bbe904
Yeray Ruiz <yeray.1997.yr@gmail.com>
2016-10-05 14:45:35 +0200

    Regex done

--- app/src/main/java/com/example/logintable/LoginTable_Activity.java
+++ app/src/main/java/com/example/logintable/LoginTable_Activity.java
@@ -2,9 +2,11 @@ package com.example.logintable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.logintable.controller.LoginTable_Controller;
 
@@ -18,6 +20,7 @@ public class LoginTable_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_table);
+
         //Initializing variables
         loginTableController = new LoginTable_Controller();
 
@@ -31,15 +34,49 @@ public class LoginTable_Activity extends AppCompatActivity {
         btCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                resetValues();
             }
         });
 
         btOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                loginClick();
             }
         });
+    }
+
+    private void loginClick() {
+        String user = etUser.getText().toString();
+        String pass = etPass.getText().toString();
+        if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)){
+            Toast.makeText(LoginTable_Activity.this,getResources().getString(R.string.data_empty),Toast.LENGTH_SHORT).show();
+        }else {
+            int result = loginTableController.validateCredentials(user,pass);
+            switch (result){
+                case 0: //Correct
+                    break;
+                case LoginTable_Controller.PASSWORD_DIGIT: //1 digit required
+                    Toast.makeText(LoginTable_Activity.this,
+                            getResources().getString(R.string.password_digit)
+                            , Toast.LENGTH_SHORT).show();
+                    break;
+                case LoginTable_Controller.PASSWORD_CASE: //1 lowercase and 1 uppercase letter required
+                    Toast.makeText(LoginTable_Activity.this,
+                            getResources().getString(R.string.password_case)
+                            , Toast.LENGTH_SHORT).show();
+                    break;
+                case LoginTable_Controller.PASSWORD_LENGTH: //Min length == 8
+                    Toast.makeText(LoginTable_Activity.this,
+                            getResources().getString(R.string.password_length)
+                            , Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+
+    private void resetValues() {
+        etPass.setText("");
+        etUser.setText("");
     }
 }
--- app/src/main/java/com/example/logintable/controller/LoginTable_Controller.java
+++ app/src/main/java/com/example/logintable/controller/LoginTable_Controller.java
@@ -1,8 +1,39 @@
 package com.example.logintable.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by usuario on 5/10/16.
  */
 
 public class LoginTable_Controller {
+
+    public static final int CORRECT = 0;
+    public static final int PASSWORD_DIGIT = 1;
+    public static final int PASSWORD_CASE = 2;
+    public static final int PASSWORD_LENGTH = 3;
+    private static final int MINLEGTH = 8;
+    private String user;
+    private String pass;
+
+    public int validateCredentials(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+        int result = 0;
+        Boolean numericMatch, uppercaseMatch, lowercaseMatch, minlength;
+        minlength = this.pass.length() < MINLEGTH;
+        numericMatch = Pattern.matches(".*[0-9]+.*",this.pass);
+        uppercaseMatch = Pattern.matches(".*[a-z]+.*",this.pass);
+        lowercaseMatch = Pattern.matches(".*[A-Z]+.*",this.pass);
+        if (minlength)
+            result = PASSWORD_LENGTH;
+        else if (!numericMatch)
+            result = PASSWORD_DIGIT;
+        else if (!uppercaseMatch)
+            result = PASSWORD_CASE;
+        else if (!lowercaseMatch)
+            result = PASSWORD_CASE;
+        return result;
+    }
 }
