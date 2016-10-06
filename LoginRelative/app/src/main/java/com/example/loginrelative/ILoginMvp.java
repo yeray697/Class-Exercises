0e1986e0bb361e83723da16e77e01ba75299e2e3
Yeray Ruiz <yeray.1997.yr@gmail.com>
2016-10-06 10:36:16 +0200

    MVP added to LoginRelative, commented, and project finished

---
+++ LoginRelative/app/src/main/java/com/example/loginrelative/ILoginMvp.java
@@ -0,0 +1,21 @@
+package com.example.loginrelative;
+
+/**
+ * Created by usuario on 6/10/16.
+ */
+
+public interface ILoginMvp {
+    int CORRECT = 0;
+    int PASSWORD_DIGIT = 1;
+    int PASSWORD_CASE = 2;
+    int PASSWORD_LENGTH = 3;
+    int DATA_EMPTY = 4;
+    int MINLENGTH = 8;
+
+    interface View{
+        public void setMessageError(String messageError);
+    }
+    interface Presenter{
+        public void validateCredentials(String user, String pass);
+    }
+}
--- LoginRelative/app/src/main/java/com/example/loginrelative/Login_Activity.java
+++ LoginRelative/app/src/main/java/com/example/loginrelative/Login_Activity.java
@@ -2,12 +2,71 @@ package com.example.loginrelative;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login_Activity extends AppCompatActivity {
+/**
+ * Class that implements a login
+ * @author Yeray Ruiz
+ * @version 1.0
+ */
+public class Login_Activity extends AppCompatActivity implements ILoginMvp.View {
+
+    private ILoginMvp.Presenter loginMvp;
+    private EditText etUser, etPass;
+    private Button btSubmit, btCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginMvp = new LoginPresenter(this); //Presenter has a reference to the view
+        //Setting controls
+        etUser = (EditText) findViewById(R.id.etUser);
+        etPass = (EditText) findViewById(R.id.etPass);
+        btSubmit = (Button) findViewById(R.id.btSubmit);
+        btCancel = (Button) findViewById(R.id.btCancel);
+        //Click events
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginClick();
+            }
+        });
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetValues();
+            }
+        });
+    }
+
+    /**
+     * Method used when user clicks on btOk
+     */
+    private void loginClick() {
+        String user = etUser.getText().toString();
+        String pass = etPass.getText().toString();
+        //Checking credentials. If there is an error, a Toast will appear
+        loginMvp.validateCredentials(user,pass);
+    }
+
+    /**
+     * Method used when user click on btCancel
+     */
+    private void resetValues() {
+        etPass.setText("");
+        etUser.setText("");
+    }
+
+    /**
+     * Method implemented that if there is an error, it will show it on a Toast
+     * @param messageError Error that will show
+     */
+    @Override
+    public void setMessageError(String messageError) {
+        Toast.makeText(this, messageError, Toast.LENGTH_SHORT).show();
     }
 }
