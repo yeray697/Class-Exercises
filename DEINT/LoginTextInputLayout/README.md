a8cde7dd323fc9d4790ce53a8876e6efdd6fe116
Yeray Ruiz <yeray.1997.yr@gmail.com>
2016-10-10 12:49:35 +0200

    New project added

---
+++ DEINT/LoginTextInputLayout/README.md
@@ -0,0 +1,9 @@
+# LoginRelative
+This project is a Login done with a RelativeLayout and implementing Model-View-Presenter.
+
+The project only check credentials (There is not a database or correct credentials):
+- It needs at least to be greater or equal than 8 characters
+- It needs at least to has a digit
+- It needs at least to has a lowercase and uppercase character
+
+Last modified: 06/10/2016
---
+++ DEINT/LoginTextInputLayout/app/src/main/java/com/example/logintextinputlayout/User.java
@@ -0,0 +1,33 @@
+package com.example.logintextinputlayout;
+
+/**
+ * Created by usuario on 6/10/16.
+ */
+
+public class User {
+    private String user;
+    private String password;
+
+    public User(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        if(this.user != user)
+            this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (this.password != password)
+            this.password = password;
+    }
+}
