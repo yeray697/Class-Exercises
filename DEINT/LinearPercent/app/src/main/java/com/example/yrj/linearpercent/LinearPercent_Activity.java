4f3de3547f8d5d6cbeda4b7874c78f32f6448e99
Yeray Ruiz <yeray.1997.yr@gmail.com>
2016-10-17 12:46:38 +0200

    New project

---
+++ DEINT/LinearPercent/app/src/main/java/com/example/yrj/linearpercent/LinearPercent_Activity.java
@@ -0,0 +1,13 @@
+package com.example.yrj.linearpercent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearPercent_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_percent);
+    }
+}
